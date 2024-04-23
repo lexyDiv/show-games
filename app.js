f53e2c4b91b27306eb31f46c65845e83a1ec6e1f
lexyDiv <aledab@mail.ru>
2024-04-23 15:25:38 +0300

    create server

---
+++ app.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`server on port ${PORT}`);
+});
