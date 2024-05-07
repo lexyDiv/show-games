658ddefa5a9df55e9d535bb2701aa64fb0cd14d5
lexyDiv <aledab@mail.ru>
2024-05-07 16:28:53 +0300

    shadow

--- components/CardsList.jsx
+++ components/CardsList.jsx
@@ -4,7 +4,7 @@ const GameCard = require('./GameCard');
 function CardList() {
   const cards = [
     {
-      title: 'Shadw of WarCraft II',
+      title: 'Shadow of WarCraft II',
       image: 'wc/title_images/first.png',
       text1: 'Реал-тайм стратегия по мотивам WarCraft II от Blizzard. Проект разрабатывается на чистом JS, без использования фреймворков и библиотек.',
       text2: 'Правая кнопка мыши в игре - это левая кнопка мыши с зажатым пробелом. Для более комфортного скроулинга экрана в игре, рекомендуется перейти в полноэкранный режим, чаще всего это F11',
